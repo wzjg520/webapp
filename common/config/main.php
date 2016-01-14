198df461c713b96677e658a9571aeafd4d0a3634
wzjg520 <wzjg520@126.com>
2016-01-14 11:35:53 +0800

    init

---
+++ common/config/main.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=app',
+            'username' => 'wzjg520',
+            'password' => 'ABC201314',
+            'charset' => 'utf-8',
+        ],
+    ],
+];
